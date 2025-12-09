67226dbdfbd693c145b706ef55c5de1fd679c09c
harumaki4649 <support@disnana.com>
2025-12-09 14:30:39 +0900

    no message

--- main.py
+++ main.py
@@ -11,3 +11,14 @@ db = NanaSQLite("test.db", bulk_load=True)
 # コンテキストマネージャ
 with NanaSQLite("test.db") as db:
     db["key"] = "value"
+    if "key" in db:
+        print("key exists")
+    else:
+        print("key does not exist")
+    if db["key"] == "value":
+        print("value is value")
+    else:
+        print("value is not value")
+    for key in db:
+        print(key)
+    
