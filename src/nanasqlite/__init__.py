579bd04eade9ba4c9416aaaeb25cf69f1d1ef019
harumaki4649 <support@disnana.com>
2025-12-17 20:59:22 +0900

    v1.1.0a1に変更しました

--- src/nanasqlite/__init__.py
+++ src/nanasqlite/__init__.py
@@ -24,6 +24,6 @@ Async Example:
 from .core import NanaSQLite
 from .async_core import AsyncNanaSQLite
 
-__version__ = "1.1.0dev3"
+__version__ = "1.1.0a1"
 __author__ = "Disnana"
 __all__ = ["NanaSQLite", "AsyncNanaSQLite"]
